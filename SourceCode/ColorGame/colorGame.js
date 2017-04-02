bb4bf8ee03bdafb4e4d240a39bf3ef019293ffcc
ugurcan.cetin <ugurcan.cetin@liferay.com>
2017-04-02 23:25:46 +0200

    Extand the color frame between 0-255 and choose randomly

--- SourceCode/ColorGame/colorGame.js
+++ SourceCode/ColorGame/colorGame.js
@@ -1,15 +1,9 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent=pickedColor;
 
@@ -26,6 +20,7 @@ for (var i = 0; i < squares.length; i++) {
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent="Correct!";
 			changeColor(clickedColor);
+			h1.style.background=pickedColor;
 		}else {
 		 this.style.background="#232323";
 		 messageDisplay.textContent="Try Again";
@@ -45,4 +40,25 @@ function pickColor(){
 
 	var random = Math.floor(Math.random()*colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make array
+	var arr = []
+	//add num random colorst to array
+	for (var i = 0; i < num; i++) {
+		arr.push(randomColor());
+	};
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" color between 0 - 255
+	var r = Math.floor(Math.random()*256);
+	//pick a "green" color between 0 - 255
+	var g = Math.floor(Math.random()*256);
+	//pick a "blue" color between 0 - 255
+	var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
